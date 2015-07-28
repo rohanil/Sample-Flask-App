397c3a782218f57103717674079345300d918825
Rohanil <rohanil112@gmail.com>
2015-07-28 16:29:04 +0900

    bootstrap sample

--- highchart/app.py
+++ highchart/app.py
@@ -1,14 +1,18 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
 Bootstrap(app)
 
-if __name__ == "__main__":
-    app.run()
-
 
 @app.route('/')
 def show_entries():
     return render_template('example.html')
+
+
+if __name__ == "__main__":
+    app.run()
+
+
